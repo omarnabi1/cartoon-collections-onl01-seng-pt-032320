091b76e25466c1b2b3d3ed1d267c9ea5222ed2b2
Omar Nabi <onabi20@gmail.com>
2020-04-01 03:34:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,14 @@ def summon_captain_planet(veggies)
 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planteer_calls(long_planteer_calls)
+  answer = false
+  long_planteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+    answer
 end
 
 def find_the_cheese# code an argument here
